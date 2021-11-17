ab1b2bff95873fa0c79ee369088b9bd7df0d9dde
denny <Denny.TL.Yang@pccw.com>
2021-11-17 08:49:16 +0800

    jboss

--- src/main/java/tw/denny/DemoApp.java
+++ src/main/java/tw/denny/DemoApp.java
@@ -13,8 +13,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class DemoApp extends SpringBootServletInitializer {
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApp.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApp.class);
     }
 
     public static void main(String[] args) {
